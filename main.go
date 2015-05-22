45ff6f8aa892d58ddc60d1589d7fb3bf8d64a2d2
Balaji Seshadri and Jean de Klerk <pair+balaji+jdeklerk@pivotal.io>
2015-05-22 14:02:13 -0600

    Small refactor

--- main.go
+++ main.go
@@ -14,18 +14,15 @@ func main() {
 	lines := strings.Split(string(data), "\n")
 	lines = lines[1:len(lines)]
 
-	var rowsAndColumns [][]string
-
-	for _, line := range lines {
-		rowsAndColumns = append(rowsAndColumns, strings.Split(line, ","))
-	}
-
 	var sql string
 
-	for _, columns := range rowsAndColumns {
+	for _, line := range lines {
+		columns := strings.Split(line, ",")
+
 		sql += fmt.Sprintf(`INSERT INTO product_service_attribute (product_id, service_id, service_type, service_name, service_reference) VALUES ('%s', '%s', '%s', '%s', '%s');
 `, columns[1], columns[3], columns[4], columns[5], columns[6])
 	}
+
 
 	fmt.Println(sql)
 
